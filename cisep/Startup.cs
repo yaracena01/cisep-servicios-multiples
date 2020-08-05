2af0930f92bf8ebd9256b99ce2def608ee6368fc
YarielPrueba <aracena.yariel@hotmail.es>
2020-08-05 08:43:39 -0400

    commit
    commit

--- cisep/Startup.cs
+++ cisep/Startup.cs
@@ -75,7 +75,7 @@ namespace cisep
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
+            if (env.IsProduction())
             {
                 app.UseDeveloperExceptionPage();
             }
